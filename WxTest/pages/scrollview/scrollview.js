a1c2385457c62c3a7284741ed9a4c0508f560652
Broken <whyefa@163.com>
2017-01-06 16:55:09 +0800

    scrollview
    Scroll-view

--- WxTest/pages/scrollview/scrollview.js
+++ WxTest/pages/scrollview/scrollview.js
@@ -1,39 +1,28 @@
+var order = ["a", "b", "c", "d"];
+var currentViewIndex = 0;
+
 Page({
   data:{
     title: "Scroll View",
     motto: "Hello WX",
     tap: "Alpha",
-    userinfo: {}
+    userinfo: {},
+    toView: "a"
   },
-  onLoad:function(options){
-    // 生命周期函数--监听页面加载
+  scrollToUpper: function() {
+    console.log("scroll to upper");
   },
-  onReady:function(){
-    // 生命周期函数--监听页面初次渲染完成
-    
-  },
-  onShow:function(){
-    // 生命周期函数--监听页面显示
-  },
-  onHide:function(){
-    // 生命周期函数--监听页面隐藏
-  },
-  onUnload:function(){
-    // 生命周期函数--监听页面卸载
-  },
-  onPullDownRefresh: function() {
-    // 页面相关事件处理函数--监听用户下拉动作
-    
-  },
-  onReachBottom: function() {
-    // 页面上拉触底事件的处理函数
-  },
-  onShareAppMessage: function() {
-    // 用户点击右上角分享
-    return {
-      title: 'title', // 分享标题
-      desc: 'desc', // 分享描述
-      path: 'path' // 分享路径
+  change: function(){
+    currentViewIndex ++;
+    if(currentViewIndex > order.length-1) {
+      currentViewIndex = 0;
     }
+    this.setData({
+      toView: order[currentViewIndex]
+    });
+  },
+  scroll: function(){
+    console.log("----scroll function----");
   }
+
 })
