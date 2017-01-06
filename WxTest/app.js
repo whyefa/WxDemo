18cc0a4e76b08013adffa0f0e59a0b744bce08f9
Broken <whyefa@163.com>
2017-01-06 10:55:56 +0800

    base
    基础内容

---
+++ WxTest/app.js
@@ -0,0 +1,47 @@
+//app.js
+App({
+  //当程序初始化的时候,执行这个函数里的代码
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  //全局的方法,
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  //全局的属性
+  globalData:{
+    userInfo:null
+  },
+  onShow: function() {
+    //小程序显示的时候触发
+  },
+  onHide:function() {
+    //小程序进入后台触发
+  },  
+  onShareAppMessage: function() {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
+    }
+  }
+})
---
+++ WxTest/pages/bindData/bindData.js
@@ -0,0 +1,38 @@
+Page({
+  data:{
+    name: "RC",
+    id: 1,
+    show: true,
+    word: "world",
+    arr: [1, 2, 3, 4, 5],
+    obj: {
+      a: 3,
+      b: 5
+    }
+  },
+  outterTap: function() {
+    console.log("outterTap")
+  },
+  middleTap:function(){
+    console.log("middleTap")
+  },
+  innerTap:function(event) {
+    console.log(event)
+    console.log("innerTap")
+  },
+  onLoad:function(options){
+    // 生命周期函数--监听页面加载
+    console.log("bindData")
+  },
+  back: function(){
+      console.log("back")
+  },
+  onShareAppMessage: function() {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
+    }
+  }
+})
